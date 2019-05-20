765ad6d02ce38749b604d72de587923c98e88aba
starrabb1t <berzin@phystech.edu>
2019-05-20 18:15:32 +0300

    add tests module
    measure fps

--- tests.py
+++ tests.py
@@ -1,41 +1,19 @@
-import cutils
-import utils
-
-import time
+from robot import Robot
+from environment import Environment
 import math
-import numpy as np
-import cv2 as cv
+import time
 
-icon = cv.imread('robot.png', cv.IMREAD_UNCHANGED)
-icon = cv.resize(icon, (50, 50), interpolation=cv.INTER_NEAREST)
+if __name__ == '__main__':
+    robot = Robot()
+    robot.set_left_speed(1)
+    robot.set_right_speed(1)
 
-#pure python
-print("pure python")
+    env = Environment()
 
-t_start = time.time()
-level = utils.getMap(200, 10, 10)
-print("getMap: " + str(time.time() - t_start))
+    position = [int(env.level.shape[0] / 2), int(env.level.shape[1] / 2), -math.pi / 2]
 
-position = np.array([int(level.shape[0] / 2), int(level.shape[1] / 2), -math.pi/2], dtype=np.uint8)
-t_start = time.time()
-utils.drawOverlay(level, icon, position)
-print("drawOverlay: " + str(time.time() - t_start))
-
-print()
-
-#cython
-print("cython")
-
-t_start = time.time()
-clevel = cutils.getMap(200, 10, 10)
-print("getMap: " + str(time.time() - t_start))
-
-cposition = np.array([int(clevel.shape[0] / 2), int(clevel.shape[1] / 2), -math.pi/2], dtype=np.uint8)
-t_start = time.time()
-cutils.drawOverlay(clevel, icon, cposition)
-print("drawOverlay: " + str(time.time() - t_start))
-
-
-#cv.imshow('frame', hello.look(arr))
-#cv.waitKey(0)
-#cv.destroyyAllWindows()
+    t_start = time.time()
+    for i in range(100):
+        position = env.updatePosition(robot, position)
+        frame = env.render(robot, position)
+    print("pure python: " + str(int(1/(0.01*(time.time() - t_start)))) + " fps")
